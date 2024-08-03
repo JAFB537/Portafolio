1aa72d02efa07656fd4701f567a88353be74722e
JAFB537 <jafb537@gmail.com>
2024-08-03 10:32:34 -0600

    Update App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainComponent,
+  ],
+  providers: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'portafolio';
+export class AppComponent implements OnInit {
+  isCollapsed: boolean;
+
+  constructor(private viewportScroller: ViewportScroller) {
+    this.isCollapsed = window.innerWidth <= 768;
+  }
+
+  ngOnInit(): void {}
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
+// app-routing.module.ts
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MainComponent }];
+
+export { routes };
